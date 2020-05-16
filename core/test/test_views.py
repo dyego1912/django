a747f4f61b5ed93d0f4996b2b9151873fccb0f93
dyego1912 <dyego1607@gmail.com>
2020-05-16 09:36:55 -0500

    Mi first commit

---
+++ core/test/test_views.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)# verifica se a página index retorna o código 200 (Acessível)
+        print(response.status_code)
+
+    def test_template_used(self):
+        
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html') #verifica se a página index está usando o template index.html
+        
+        
---
+++ core/views.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    #return HttpResponse('Hello, World!!')
+    
+    return render(request, 'index.html')
+
+''' 
+   texts = ['Esse é um site sobre', 'Django', 'Você sabe o que é', 'Django?']
+    
+    context = {
+        'title':'Django E-Commerce',
+        'texts': texts
+    }
+
+    return render(request, 'index.html', context) #procura o arquivo index.html dentro do diretório core/templates 
+'''
+    
+
+def base(request):
+      
+    return render(request, 'base.html')
+
+def contact(request):
+      
+    return render(request, 'contact.html')
+
+def product(request):
+      
+    return render(request, 'product.html')
+
+def product_list(request):
+      
+    return render(request, 'product_list.html')
+
+def warning(request):
+    
+    return render(request, 'warning.html')
